55a17b86d3d9e6feeef762dcff4d568e3f292e40
GudiSrikanth <91872346+GudiSrikanth@users.noreply.github.com>
2022-06-15 20:47:31 +0530

    Revert "commit by ganesh"
    This reverts commit e259313ff7bc79c2fc5baa6d27312a77eae74978.

--- application/src/main/java/com/ecommerce/application/configuration/WebSecurityConfig.java
+++ application/src/main/java/com/ecommerce/application/configuration/WebSecurityConfig.java
@@ -48,12 +48,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/users").authenticated()
-            .anyRequest().permitAll()
+            .antMatchers("/user")
+            .permitAll()
             .and()
             .formLogin()
                 .usernameParameter("email")
-                .passwordParameter("parameter")
                 .defaultSuccessUrl("/categories")
                 .permitAll()
             .and()
