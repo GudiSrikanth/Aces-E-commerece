638e4455aa57185ff33ddd67b893d4092ed73005
Kalyan891 <kandulakalyan1436@gmail.com>
2022-06-20 15:15:21 +0530

    commit by kalyan

--- application/src/main/java/com/ecommerce/application/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.application.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
- 
-}
